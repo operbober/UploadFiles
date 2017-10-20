d76a5c4a1ab55cd8ca04eaed5c1f3a072efde739
Ivan Malets <operbober@mail.ru>
2017-10-20 20:07:08 +0300

    update configs

--- config/index.js
+++ config/index.js
@@ -1,16 +1,10 @@
 module.exports = {
     "server": {
-        "ipAddress": process.env.OPENSHIFT_NODEJS_IP || "192.168.1.3",
-        "port": process.env.OPENSHIFT_NODEJS_PORT ||3000
+        "ipAddress": "192.168.1.3",
+        "port": 3000
     },
     "mongoose" : {
-        "url": process.env.OPENSHIFT_MONGODB_DB_PASSWORD
-            ? connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-                process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-                process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-                process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-                process.env.OPENSHIFT_APP_NAME
-            : "mongodb://192.168.1.3/upload"
+        "url":"mongodb://192.168.1.3/upload"
     },
     "session": {
         "secret": "ololotrololo9voditelnlo",
